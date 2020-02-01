226cbe98dec29dd4202cbf07430c2cf495f27e50
etnestad <etnestad@gmail.com>
2020-02-01 10:13:51 +0100

    Added test option

--- start.py
+++ start.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 import subprocess
 import os
-import time
+import sys
+
+test_run = 0
+
+if len(sys.argv) == 2 and sys.argv[1] == "test":
+    print("TEST RUN!")
+    test_run = 1
 
 my_env = os.environ.copy()
 my_env['DISPLAY'] = ":1"
@@ -9,7 +15,10 @@ my_env['WINEDEBUG'] = "-all"
 
 xvfb = subprocess.Popen(["Xvfb",":1","-screen","0"," 1600x1200x24"],env=my_env)
 
-wine = subprocess.Popen(["script","--return","--quiet","-c wine python 'c:\cdsc\dscontrol.py'","/home/audun/logg"],env=my_env)
+if test_run == 1:
+    wine = subprocess.Popen(["script","--return","--quiet","-c wine python 'c:\cdsc\dscontrol.py' test","/home/audun/logg"],env=my_env)
+else:
+    wine = subprocess.Popen(["script","--return","--quiet","-c wine python 'c:\cdsc\dscontrol.py'","/home/audun/logg"],env=my_env)
 
 x11vnc = subprocess.Popen(["x11vnc","-display",":1","-nopw","-quiet","-forever"])
 
