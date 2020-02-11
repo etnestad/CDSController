c1068da9fe2e9ae6954d4053c89e3dc6e6f4e089
etnestad <etnestad@gmail.com>
2020-02-11 23:15:42 +0100

    Cleanup

--- start.py
+++ start.py
@@ -13,15 +13,15 @@ my_env = os.environ.copy()
 my_env['DISPLAY'] = ":1"
 my_env['WINEDEBUG'] = "-all"
 
-xvfb = subprocess.Popen(["Xvfb",":1","-screen","0"," 1600x1200x24"],env=my_env)
+xvfb = subprocess.Popen(["Xvfb",my_env['DISPLAY'],"-screen","0"," 1600x1200x24"],env=my_env)
 
 if test_run == 1:
     wine = subprocess.Popen(["script","--return","--quiet","-c wine python 'c:\cdsc\dscontrol.py' test","/home/audun/logg"],env=my_env)
 else:
     wine = subprocess.Popen(["script","--return","--quiet","-c wine python 'c:\cdsc\dscontrol.py'","/home/audun/logg"],env=my_env)
 
-x11vnc = subprocess.Popen(["x11vnc","-display",":1","-nopw","-quiet","-forever"])
+#x11vnc = subprocess.Popen(["x11vnc","-display",":1","-nopw","-quiet","-forever"])
 
 wine.wait()
-x11vnc.kill()
+#x11vnc.kill()
 xvfb.kill()
